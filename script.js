13671af8368d7314999732a22fe8f48f207c9f26
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-12 17:52:06 -0300

    Requisito 10

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const paletteSquares = document.querySelectorAll('.color');
-const pixel = document.querySelectorAll('.pixel');
+const pixelBoard = document.getElementById('pixel-board');
+
 const colors = ['black', '#297373', '#ff8552', '#e9d758'];
 const button = document.getElementById('clear-board');
 const input = document.getElementById('board-size');
@@ -7,6 +8,21 @@ const sizeButton = document.getElementById('generate-board');
 
 paletteSquares[0].classList.add('selected');
 
+function generateBoard(size = 5) {
+  for (let index = 0; index < size; index += 1) {
+    const tag = document.createElement('ul');
+    pixelBoard.appendChild(tag);
+    for (let counter = 0; counter < size; counter += 1) {
+      const liPixels = document.createElement('li');
+      liPixels.className = 'pixel';
+      pixelBoard.children[index].appendChild(liPixels);
+    }
+  }
+}
+
+generateBoard();
+
+const pixel = document.querySelectorAll('.pixel');
 // Pedi ajuda por Discord do meu amigo Douglas Oliveira para resolver questão de for dentro de uma function
 // GitHub: https://github.com/dg-os
 // Linkedin: https://www.linkedin.com/in/douglas-oliveira-5a03b5b9/
@@ -46,14 +62,20 @@ button.addEventListener('click', clearPixel);
 sizeButton.addEventListener('click', () => {
   if (input.value === '') {
     alert('Board inválido!');
+  } else {
+    pixel.forEach((item) => {
+      const remove = document.querySelectorAll('.pixel');
+      remove.forEach((element) => {
+        element.remove();
+      });
+      const moment = item;
+      const sizeInt = parseInt(input.value, 0);
+      if (sizeInt >= 5 && sizeInt <= 50) {
+        generateBoard(sizeInt);
+        moment.style.width = `${input.value}px`;
+        moment.style.height = `${input.value}px`;
+        clearPixel();
+      }
+    });
   }
-  pixel.forEach((item) => {
-    const moment = item;
-    const sizeInt = parseInt(input.value, 0);
-    if (sizeInt >= 5 && sizeInt <= 50) {
-      moment.style.width = `${input.value}px`;
-      moment.style.height = `${input.value}px`;
-      clearPixel();
-    }
-  });
 });
