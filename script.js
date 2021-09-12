8b087c86981eef1452240c9e037e195ea243ebb2
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-12 19:06:27 -0300

    Requisito 11

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const paletteSquares = document.querySelectorAll('.color');
 const pixelBoard = document.getElementById('pixel-board');
-
 const colors = ['black', '#297373', '#ff8552', '#e9d758'];
 const button = document.getElementById('clear-board');
 const input = document.getElementById('board-size');
@@ -8,14 +7,25 @@ const sizeButton = document.getElementById('generate-board');
 
 paletteSquares[0].classList.add('selected');
 
+function paintSquare(square) {
+  const moment = square;
+  for (let index = 0; index < paletteSquares.length; index += 1) {
+    if (paletteSquares[index].classList.contains('selected')) {
+      moment.target.style.backgroundColor = colors[index];
+    }
+  }
+}
+
 function generateBoard(size = 5) {
   for (let index = 0; index < size; index += 1) {
-    const tag = document.createElement('ul');
+    const tag = document.createElement('div');
     pixelBoard.appendChild(tag);
     for (let counter = 0; counter < size; counter += 1) {
-      const liPixels = document.createElement('li');
+      const liPixels = document.createElement('div');
       liPixels.className = 'pixel';
       pixelBoard.children[index].appendChild(liPixels);
+      // O João (Spinelli) Gabriel - Turma 16 - A me ajudou a colocar o escutador dentro da função de geração do board
+      liPixels.addEventListener('click', paintSquare);
     }
   }
 }
@@ -39,17 +49,6 @@ for (let index = 0; index < paletteSquares.length; index += 1) {
   paletteSquares[index].style.backgroundColor = colors[index];
 }
 
-pixel.forEach((item) => {
-  item.addEventListener('click', (event) => {
-    const moment = event;
-    for (let index = 0; index < paletteSquares.length; index += 1) {
-      if (paletteSquares[index].classList.contains('selected')) {
-        moment.target.style.backgroundColor = colors[index];
-      }
-    }
-  });
-});
-
 function clearPixel() {
   pixel.forEach((item) => {
     const element = item;
@@ -59,23 +58,31 @@ function clearPixel() {
 
 button.addEventListener('click', clearPixel);
 
+function clearBoard() {
+  pixel.forEach(() => {
+    const remove = document.querySelectorAll('.pixel');
+    remove.forEach((element) => {
+      element.parentNode.remove();
+    });
+  });
+}
+
+function sizeErrorValue(size) {
+  if (size < 5) {
+    return 5;
+  }
+  if (size > 50) {
+    return 50;
+  }
+  return size;
+}
+
 sizeButton.addEventListener('click', () => {
   if (input.value === '') {
     alert('Board inválido!');
   } else {
-    pixel.forEach((item) => {
-      const remove = document.querySelectorAll('.pixel');
-      remove.forEach((element) => {
-        element.remove();
-      });
-      const moment = item;
-      const sizeInt = parseInt(input.value, 0);
-      if (sizeInt >= 5 && sizeInt <= 50) {
-        generateBoard(sizeInt);
-        moment.style.width = `${input.value}px`;
-        moment.style.height = `${input.value}px`;
-        clearPixel();
-      }
-    });
+    clearBoard();
+    const sizeInt = parseInt(input.value, 0);
+    generateBoard(sizeErrorValue(sizeInt));
   }
 });
