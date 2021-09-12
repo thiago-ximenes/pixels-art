46df4407928a07784e2758617f556131a28bd8e0
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-11 23:21:40 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 /* eslint-disable no-restricted-syntax */
 const paletteSquares = document.querySelectorAll('.color');
+const pixel = document.querySelectorAll('.pixel');
+// const selectItems = document.querySelectorAll('.selected');
+const colors = ['black', '#297373', '#ff8552', '#e9d758'];
 
 paletteSquares[0].classList.add('selected');
 
@@ -9,5 +12,23 @@ paletteSquares.forEach((item) => {
       element.classList.remove('selected');
     }
     event.target.classList.add('selected');
+    document.querySelector('.selected').style.backgroundColor =
+      event.target.style.backgroundColor;
+  });
+});
+
+for (let index = 0; index < paletteSquares.length; index += 1) {
+  paletteSquares[index].style.backgroundColor = colors[index];
+}
+
+pixel.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    const moment = event;
+    for (const key in paletteSquares) {
+      if (paletteSquares[key].classList.contains('selected')) {
+        moment.target.style.backgroundColor = colors[key];
+      }
+    }
+    // event.target.style.backgroundColor = colors[1];
   });
 });
