20d92f592c3eab8ddc42c69763aa9437ab1c9a84
Thiago Ximenes <thiagoximenes@arch.localdomain>
2021-09-12 12:19:29 -0300

    Corrige linter do for

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const paletteSquares = document.querySelectorAll('.color');
 const pixel = document.querySelectorAll('.pixel');
 const colors = ['black', '#297373', '#ff8552', '#e9d758'];
 const button = document.getElementById('clear-board');
+const input = document.getElementById('board-size');
+const sizeButton = document.getElementById('generate-board');
 
 paletteSquares[0].classList.add('selected');
 
+// Douglas, amigo meu, me ajudou
 paletteSquares.forEach((item) => {
   item.addEventListener('click', (event) => {
-    for (const element of paletteSquares) {
+    paletteSquares.forEach((element) => {
       element.classList.remove('selected');
-    }
+    });
     event.target.classList.add('selected');
   });
 });
@@ -33,5 +36,13 @@ pixel.forEach((item) => {
   button.addEventListener('click', () => {
     const element = item;
     element.style.backgroundColor = 'white';
+  });
+});
+
+pixel.forEach((item) => {
+  sizeButton.addEventListener('click', () => {
+    const moment = item;
+    moment.style.width = `${input.value}px`;
+    moment.style.height = `${input.value}px`;
   });
 });
